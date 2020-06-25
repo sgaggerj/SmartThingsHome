80c23b44d331e90ebb5bf3cc86e914c318ac9001
Jon Sgaggero <sgaggerj@users.noreply.github.com>
2020-06-25 19:12:26 -0400

    Update denon-avr-old.groovy

--- devicetypes/sgaggerj/denon-avr-old.src/denon-avr-old.groovy
+++ devicetypes/sgaggerj/denon-avr-old.src/denon-avr-old.groovy
@@ -86,11 +86,12 @@ preferences {
     //tiles {
 	tiles(scale: 2) {
 		multiAttributeTile(name:"multiAVR", type: "mediaPlayer", width: 6, height: 4) {
-/**          
+          
 		  tileAttribute("device.status", key: "PRIMARY_CONTROL") { 	            
             	attributeState ("paused", label: 'Paused', backgroundColor: "#53a7c0", defaultState: true)
 				attributeState ("playing", label: 'Playing', backgroundColor: "#79b821")
         	}             
+/**
             tileAttribute("device.status", key: "MEDIA_STATUS") { 	            
             	attributeState "playing", label: '${name}', action:"switch.off"
                 attributeState "paused", label: '${name}', action:"switch.on"
